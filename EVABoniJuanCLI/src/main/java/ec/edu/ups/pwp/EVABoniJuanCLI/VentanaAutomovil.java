90f412f28af0de068f0740955cc05b0048bb5299
juanboni <52544365+juanboni@users.noreply.github.com>
2022-02-01 13:07:49 -0500

    commit prueba

--- EVABoniJuanCLI/src/main/java/ec/edu/ups/pwp/EVABoniJuanCLI/VentanaAutomovil.java
+++ EVABoniJuanCLI/src/main/java/ec/edu/ups/pwp/EVABoniJuanCLI/VentanaAutomovil.java
@@ -9,10 +9,16 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
 
 public class VentanaAutomovil extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField placa;
+	private JTextField modelo;
+	private JTextField marca;
+	private JTextField tipo;
 
 	/**
 	 * Launch the application.
@@ -38,30 +44,68 @@ public class VentanaAutomovil extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 		
-		JButton btnNewButton = new JButton("New button");
-		btnNewButton.addActionListener(new ActionListener() {
+		JButton crear = new JButton("crear");
+		crear.setBounds(182, 195, 90, 38);
+		crear.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				//AutomovilesServiceSOAPService auto=new AutomovilesServiceSOAPService();
 				//auto.AutomovilesServiceSOAPPo
 				
 				AutomovilesServiceSOAPService ss = new AutomovilesServiceSOAPService();
 		        AutomovilesServiceSOAP port = ss.getAutomovilesServiceSOAPPort();
+		        
+		        
 				
 				 System.out.println("Invoking crearAutomovil...");
 			        ec.edu.ups.pwp.EVABoniJuanCLI.Automovil auto = null;
 			        auto=new Automovil();
-			        auto.setPlaca("ABI2222");
-			        auto.setPlaca("Toyota");
-			        auto.setModelo("YARIS");
-			        auto.setTipo("Camioneta");
+			        auto.setPlaca(placa.getText());
+			        auto.setMarca(marca.getText());
+			        auto.setModelo(modelo.getText());
+			        auto.setTipo(tipo.getText());
 			        java.lang.String _crearAutomovil__return = port.crearAutomovil(auto);
 			        System.out.println("crearAutomovil.result=" + _crearAutomovil__return);
 			}
 		});
-		contentPane.add(btnNewButton, BorderLayout.WEST);
+		contentPane.setLayout(null);
+		contentPane.add(crear);
+		
+		placa = new JTextField();
+		placa.setBounds(176, 77, 96, 19);
+		contentPane.add(placa);
+		placa.setColumns(10);
+		
+		JLabel Placa = new JLabel("Placa");
+		Placa.setBounds(104, 80, 45, 13);
+		contentPane.add(Placa);
+		
+		JLabel lblModelo = new JLabel("modelo");
+		lblModelo.setBounds(104, 103, 45, 13);
+		contentPane.add(lblModelo);
+		
+		JLabel lblMarca = new JLabel("Marca");
+		lblMarca.setBounds(104, 133, 45, 13);
+		contentPane.add(lblMarca);
+		
+		JLabel lblTipo = new JLabel("Tipo");
+		lblTipo.setBounds(104, 162, 45, 13);
+		contentPane.add(lblTipo);
+		
+		modelo = new JTextField();
+		modelo.setColumns(10);
+		modelo.setBounds(176, 100, 96, 19);
+		contentPane.add(modelo);
+		
+		marca = new JTextField();
+		marca.setColumns(10);
+		marca.setBounds(176, 130, 96, 19);
+		contentPane.add(marca);
+		
+		tipo = new JTextField();
+		tipo.setColumns(10);
+		tipo.setBounds(176, 159, 96, 19);
+		contentPane.add(tipo);
 	}
-
 }
